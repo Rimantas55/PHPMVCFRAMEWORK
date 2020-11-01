eda8abd0583fcf7962f343f9719808aa77fb252c
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2020-11-01 22:36:35 +0200

    testing if router works - it works

--- public/index.php
+++ public/index.php
@@ -4,3 +4,11 @@
  //? echo 'Current PHP version: ' . phpversion();
  //? PHP version: 7.2.10
  //? How to catch Request URL: // echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+ /**
+  * Routing
+  */
+require '../Core/Router.php';
+$router = new Router();
+
+echo get_class($router);
