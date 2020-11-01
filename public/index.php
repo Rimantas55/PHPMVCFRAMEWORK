4a7c1c9992ff923ed1a241345f2b45f12f4f0b82
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2020-11-01 22:20:33 +0200

    added .htaccess file to make prettier URL

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo "Hello world from public folder";
+/* Front controller */
+ //? echo 'Current PHP version: ' . phpversion();
+ //? PHP version: 7.2.10
+
+ echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
