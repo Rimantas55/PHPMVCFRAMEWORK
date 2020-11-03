4a11b707c297a1c2916512bde77a815336215217
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2020-11-03 20:54:02 +0200

    fixed reular expression

--- Core/Router.php
+++ Core/Router.php
@@ -2,9 +2,12 @@
 
 /**
  * Router
+ *
+ * PHP version 5.4
  */
 class Router
 {
+
     /**
      * Associative array of routes (the routing table)
      * @var array
@@ -25,8 +28,17 @@ class Router
      *
      * @return void
      */
-    public function add($route, $params)
+    public function add($route, $params = [])
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
@@ -41,49 +53,44 @@ class Router
     }
 
     /**
-     * Match the route to the routes in the routing table , setting the $params
-     * property if a route is found
-     * 
+     * Match the route to the routes in the routing table, setting the $params
+     * property if a route is found.
+     *
      * @param string $url The route URL
-     * 
-     * @return boolean true if match foud, false otherwise
+     *
+     * @return boolean  true if a match found, false otherwise
      */
     public function match($url)
     {
-    /*   foreach($this->routes as $route => $params) {
-        if ($url == $route) {
-          $this->params = $params;
-          return true;
-        }
-      }
-      return false; */
-    }
-      // Match to the fixed URL format /controller/action
-      public function aaa($url) {
-      $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)/";
-      if (preg_match($reg_exp, $url, $matches)) {
-        // Get Named capture group values
-        $params = [];
+        // Match to the fixed URL format /controller/action
+        //$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
 
-        foreach ($matches as $key => $match) {
-          if (is_string($key)) {
-            $params[$key] = $match;
-          }
-        }
-        $this->params = $params;
-        return true;
-      }
-    }
-    
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                // Get named capture group values
+                //$params = [];
 
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     /**
      * Get the currently matched parameters
-     * 
+     *
      * @return array
      */
     public function getParams()
     {
-      return $this->params;
+        return $this->params;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,34 +1,39 @@
 <?php
 
-/* Front controller */
- //? echo 'Current PHP version: ' . phpversion();
- //? PHP version: 7.2.10
- //? How to catch Request URL: // echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+/**
+ * Front controller
+ *
+ * PHP version 5.4
+ */
 
- /**
-  * Routing
-  */
+/**
+ * Routing
+ */
 require '../Core/Router.php';
-$router = new Router();
 
-// echo get_class($router);
+$router = new Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
     
 // Display the routing table
-// echo '<pre>';
-// var_dump($router->getRoutes());
-// echo '</pre>';
+echo '<pre>';
+//var_dump($router->getRoutes());
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
- $url = $_SERVER['QUERY_STRING'];
 
- if ($router->match($url)) {
-  echo '<pre>' . "---------------</br>";
-  var_dump($router->getParams());
-  echo '</pre>';
- } else {
-   echo "No route found for URL '$url'" ; // later will be error 404
- }
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
