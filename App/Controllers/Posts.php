ce62f17f045bacd23952145707c25761d08b458f
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2020-11-07 14:03:32 +0200

    Remove query string variables from the URL before matching to a route

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -1,28 +1,15 @@
 <?php
+
 namespace App\Controllers;
-/**
- * Posts controller
- *
- * PHP version 5.4
- */
 class Posts
 {
-
-    /**
-     * Show the index page
-     *
-     * @return void
-     */
     public function index()
     {
         echo 'Hello from the index action in the Posts controller!';
+        echo '<pre>Query string parameters: <pre>' . 
+            htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
     }
 
-    /**
-     * Show the add new page
-     *
-     * @return void
-     */
     public function addNew()
     {
         echo 'Hello from the addNew action in the Posts controller!';
